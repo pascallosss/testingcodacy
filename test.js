8a273da651ae9c7a01e940b75c325ef91bb0d188
pascallosss <53646294+pascallosss@users.noreply.github.com>
2019-08-03 12:25:33 +0200

    Update test.js

--- test.js
+++ test.js
@@ -16,3 +16,23 @@ var geen_camelcase = "en geen quotes";
 if (temp == geen_camelcase) return true;
 
 console.log('kaakakakakak');
+
+
+var kaas = 'aaaa';
+
+// todo: hallo!!!
+
+var nunietquotes = "alalalalal";
+
+console.log('test');
+
+
+var temp = 'test';
+
+var geen_camelcase = "en geen quotes";
+
+// TODO waarom?
+
+if (temp == geen_camelcase) return true;
+
+console.log('kaakakakakak');
